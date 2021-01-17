3514eba173dd119be2f00e2adf786228d8c9b2ae
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2021-01-16 20:13:22 -0800

    php

--- index.php
+++ index.php
@@ -16,13 +16,10 @@ switch ($action){
         include 'views/registration.php';
         break;
     case 'medical':
-        $_SESSION["title"] = "Kosapacha Group Medical Page";
-        $_SESSION["h1"] = "Medical products";
         include 'views/medical-products.php';
         break;
     default:
-        $_SESSION["title"] = "Kosapacha Group Home Page";
-        $_SESSION["h1"] = "Kosapacha Group";
-        include '/views/home.php';
+        include 'views/home.php';
         break;
 }
+?>
