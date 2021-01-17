dd9ba38e11de5ae453fe88ac7bf9884209a536d3
roguejuan44 <50275473+roguejuan44@users.noreply.github.com>
2021-01-16 20:43:05 -0800

    one more try

--- index.php
+++ index.php
@@ -1,29 +1,24 @@
 <?php
 
-echo "one";
 // Create or access a Session
-session_start();
+//session_start();
 
-echo "two";
 
 $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
     if ($action == NULL){
         $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
  }
 
-echo "three";    
+
 switch ($action){
     case 'register-page':
-        echo "four";
         include 'views/registration.php';
         break;
     case 'medical':
-        echo "five";
         include 'views/medical-products.php';
         break;
     default:
         include 'views/home.php';
-        echo "meow";
         break;
 }
 
